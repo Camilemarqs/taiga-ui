ce5b4f2d6fec9147736b745a0f0cffe70004529e
Werley <franciscowerley08@gmail.com>
2026-01-12 21:46:28 -0300

    refactor(table-control.directive): remove code smell, of the IIC type

--- projects/addon-table/directives/table-control/table-control.directive.ts
+++ projects/addon-table/directives/table-control/table-control.directive.ts
@@ -1,6 +1,7 @@
-import {computed, Directive, type Signal, signal} from '@angular/core';
-import {TuiControl} from '@taiga-ui/cdk/classes';
-import {tuiFallbackValueProvider} from '@taiga-ui/cdk/tokens';
+import {computed, Directive, inject, type Signal, signal, untracked} from '@angular/core';
+import {type ControlValueAccessor, NgControl, NgModel} from '@angular/forms';
+import {EMPTY_FUNCTION} from '@taiga-ui/cdk/constants';
+import {tuiFallbackValueProvider, TUI_FALLBACK_VALUE} from '@taiga-ui/cdk/tokens';
 import {tuiArrayToggle} from '@taiga-ui/cdk/utils/miscellaneous';
 
 import {type TuiCheckboxRowDirective} from './checkbox-row.directive';
@@ -9,12 +10,24 @@ import {type TuiCheckboxRowDirective} from './checkbox-row.directive';
     selector: '[tuiTable][ngModel],[tuiTable][formControl],[tuiTable][formControlName]',
     providers: [tuiFallbackValueProvider([])],
 })
-export class TuiTableControlDirective<T> extends TuiControl<readonly T[]> {
+export class TuiTableControlDirective<T> implements ControlValueAccessor {
+    private readonly fallback = inject(TUI_FALLBACK_VALUE, {self: true, optional: true}) as readonly T[];
+    private readonly control = inject(NgControl, {self: true});
+    private readonly internal = signal<readonly T[]>(this.fallback || []);
     private readonly children = signal<ReadonlyArray<TuiCheckboxRowDirective<T>>>([]);
+
+    public readonly value = computed(() => this.internal() ?? (this.fallback || []));
 
     public readonly checked: Signal<boolean> = computed(() => this.isAllChecked());
 
     public readonly indeterminate: Signal<boolean> = computed(() => this.hasIndeterminateState());
+
+    public onChange: (value: readonly T[]) => void = EMPTY_FUNCTION;
+    public onTouched = EMPTY_FUNCTION;
+
+    constructor() {
+        this.control.valueAccessor = this;
+    }
 
     public toggleAll(): void {
         this.onChange(
@@ -25,6 +38,30 @@ export class TuiTableControlDirective<T> extends TuiControl<readonly T[]> {
     public process(checkbox: TuiCheckboxRowDirective<T>): void {
         this.children.update((children) => tuiArrayToggle(children, checkbox));
     }
+
+    public writeValue(value: readonly T[] | null): void {
+        const safe = this.control instanceof NgModel ? this.control.model : value;
+        this.internal.set((safe as readonly T[]) ?? this.fallback);
+    }
+
+    public registerOnChange(onChange: (value: unknown) => void): void {
+        this.onChange = (value: readonly T[]) => {
+            const internal = untracked(() => this.internal());
+
+            if (value === internal) {
+                return;
+            }
+
+            onChange(value);
+            this.internal.set(value);
+        };
+    }
+
+    public registerOnTouched(onTouched: () => void): void {
+        this.onTouched = onTouched;
+    }
+
+    public setDisabledState(): void {}
 
     private isAllChecked(): boolean {
         const childrenList = this.children();
