a4feddf3bd7a3baa620c0da0a1214b4df820740d
Werley <franciscowerley08@gmail.com>
2026-01-12 21:13:19 -0300

    refactor(unmask-handler.directive): remove code smell, of the ICC type

--- projects/kit/directives/unmask-handler/unmask-handler.directive.ts
+++ projects/kit/directives/unmask-handler/unmask-handler.directive.ts
@@ -18,13 +18,20 @@ export class TuiUnmaskHandler extends TuiValueTransformer<string> {
     public readonly maskito = input<MaskitoOptions | null>(null);
 
     public override fromControlValue(controlValue: unknown): string {
-        return maskitoTransform(
-            String(controlValue ?? ''),
-            this.maskito() || MASKITO_DEFAULT_OPTIONS,
-        );
+        return this.transformWithMaskito(controlValue);
     }
 
     public override toControlValue(value: string): string {
+        return this.applyUnmaskHandler(value);
+    }
+
+    private transformWithMaskito(controlValue: unknown): string {
+        const stringValue = String(controlValue ?? '');
+        const maskOptions = this.maskito() || MASKITO_DEFAULT_OPTIONS;
+        return maskitoTransform(stringValue, maskOptions);
+    }
+
+    private applyUnmaskHandler(value: string): string {
         return this.tuiUnmaskHandler()(value);
     }
 }
