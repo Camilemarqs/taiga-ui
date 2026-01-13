2b8d2dc8879937b5b61567fe79b2050b359fff54
Werley <franciscowerley08@gmail.com>
2026-01-12 23:13:12 -0300

    refactor(directive-binding.ts): remove code smell, of the ANY type

--- projects/cdk/utils/di/directive-binding.ts
+++ projects/cdk/utils/di/directive-binding.ts
@@ -14,42 +14,51 @@ type SignalLikeTypeOf<T> = T extends Signal<infer R> ? R : T;
 
 type SignalLike<T> = Signal<T> | T;
 
+type ValueType<T, G extends keyof T> = SignalLikeTypeOf<T[G]>;
+
 // TODO: Consider {} as default options instead
 export function tuiDirectiveBinding<
     T,
     G extends keyof T,
-    I extends SignalLike<SignalLikeTypeOf<T[G]>>,
+    I extends SignalLike<ValueType<T, G>>,
 >(
     token: ProviderToken<T>,
     key: G,
     initial: I,
     options: InjectOptions = {self: true},
-): I extends Signal<any> ? I : WritableSignal<I> {
-    const result: any = isSignal(initial) ? initial : signal(initial);
-    const directive: any = inject(token, options);
-    const output = directive?.[`${key.toString()}Change`];
+): I extends Signal<ValueType<T, G>> ? I : WritableSignal<ValueType<T, G>> {
+    type V = ValueType<T, G>;
+    const result: I extends Signal<V> ? I : WritableSignal<V> = isSignal(initial)
+        ? (initial as I extends Signal<V> ? I : never)
+        : (signal(initial) as WritableSignal<V>);
+    const directive: T | null = inject(token, options);
+    const output = directive?.[`${key.toString()}Change` as keyof T] as
+        | {emit: (value: V) => void}
+        | undefined;
 
     if (!directive) {
         return result;
     }
 
     // TODO: Figure out why effects are executed all the time and not just when result changes (check with Angular 18)
-    let previous: any;
+    let previous: V | undefined;
 
     effect(() => {
-        const value: any = result();
+        const value: V = result() as V;
 
         if (previous === value) {
             return;
         }
 
-        if (isSignal(directive[key])) {
-            tuiSetSignal(directive[key] as any, value);
+        const directiveKey = directive[key];
+        if (isSignal(directiveKey)) {
+            tuiSetSignal(directiveKey as WritableSignal<V> | Signal<V>, value);
         } else {
-            directive[key] = value;
+            (directive as Record<keyof T, V>)[key] = value;
         }
 
-        directive.ngOnChanges?.({});
+        (directive as {ngOnChanges?: (changes: Record<string, unknown>) => void})
+            .ngOnChanges?.({});
         output?.emit?.(value);
         previous = value;
     });
