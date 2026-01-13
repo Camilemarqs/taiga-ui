51e5db8e11e6636a0432a839e57d8ca2f6dd01b9
Werley <franciscowerley08@gmail.com>
2026-01-12 21:52:22 -0300

    refactor(generic-filter.directive.ts): remove code smell, of the IIC type

--- projects/addon-table/directives/table-filters/generic-filter.directive.ts
+++ projects/addon-table/directives/table-filters/generic-filter.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, input} from '@angular/core';
+import {Directive, input, type Signal} from '@angular/core';
 import {TUI_TRUE_HANDLER} from '@taiga-ui/cdk/constants';
 import {tuiProvide} from '@taiga-ui/cdk/utils/di';
 
@@ -8,8 +8,8 @@ import {AbstractTuiTableFilter} from './abstract-table-filter';
     selector: '[tuiGenericFilter]',
     providers: [tuiProvide(AbstractTuiTableFilter, TuiGenericFilter)],
 })
-export class TuiGenericFilter<T, G> extends AbstractTuiTableFilter<T, G> {
-    public readonly filter = input<(item: T, value: G) => boolean>(TUI_TRUE_HANDLER, {
+export class TuiGenericFilter<T, G> implements AbstractTuiTableFilter<T, G> {
+    public readonly filter: Signal<(item: T, value: G) => boolean> = input<(item: T, value: G) => boolean>(TUI_TRUE_HANDLER, {
         alias: 'tuiGenericFilter',
     });
 }
